73ebc64b02da39d1f11dc41f06db024893ecd729
Lukáš Moravec <lukas.moravec@mbi.io>
2026-02-10 10:06:37 +0100

    HEUREKA-218 add missing exports

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,10 @@ import type { IAzureAuthConfig } from './connectors/azure/interfaces';
 
 export * from './connectors/azure/interfaces';
 export * from './service/interfaces.pbi';
+export * from './service/interfaces.fabric';
 export * from './service/powerBI.interfaces';
 export * from './service/dto/powerBiConfig.dto';
+export * from './service/enums';
 
 export interface PowerBiConfig {
   azureConfig: IAzureAuthConfig;
